35ac863f812a4ddab541a2298343f71a1657ce0e
ZEEstranger <bidmexov98@gmail.com>
2025-05-25 23:36:39 +0300

    Added team's name and creation date parsing function

--- parsing_team.py
+++ parsing_team.py
@@ -1,6 +1,7 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page
+import re
 
-def main():
+def parse_team_links() -> set:
     base_url = "https://liquipedia.net"
     teams_url = base_url + "/leagueoflegends/Portal:Teams"
 
@@ -18,7 +19,7 @@ def main():
         league_panels = page.query_selector_all("div.panel-box")
 
         for league_panel in league_panels:
-            team_league = league_panel.query_selector("div.panel-box-heading.a").query_selector("a").inner_text()
+            team_league = league_panel.query_selector("div.panel-box-heading").query_selector("a").inner_text()
             print(team_league)
             team_names = league_panel.query_selector_all("span.team-template-text")
             for team_name in team_names:
@@ -34,5 +35,54 @@ def main():
 
         browser.close()
 
+        return team_links, page
+
+def parse_team_info(page: Page, url):
+    base_url = "https://liquipedia.net"
+    page.goto(base_url + url)
+    page.wait_for_selector("div.fo-nttax-infobox-wrapper")
+
+    name = None
+    region = None
+    founded = None
+    disbanded = None
+
+    name = page.query_selector("div.infobox-header")
+    
+    name = name.inner_text()
+    cleaned = re.sub(r"\[.*?\]", "", name)
+    name = re.sub(r"\s+", " ", cleaned).strip()
+
+    section_counter = 1
+
+
+    date_box = ''
+    while date_box != 'History':
+        date_box = page.query_selector(f"div.fo-nttax-infobox > div:nth-of-type({section_counter}) > div")
+        date_box = date_box.inner_text()
+        section_counter += 1
+
+    date_box = page.query_selector(f"div.fo-nttax-infobox > div:nth-of-type({section_counter}) > div:nth-of-type(2)")
+    date_box = date_box.inner_text()[-10:]
+
+
+    print(name, date_box)
+
+
 if __name__ == "__main__":
-    main()
+    team_links, pw_page = parse_team_links()
+    
+    with sync_playwright() as pw:
+        browser = pw.chromium.launch(headless=True)
+        page = browser.new_page()
+
+
+        cntr = 0
+        for team_link in team_links:
+            cntr += 1
+            if not cntr % 5:
+                print(cntr)
+            a = parse_team_info(page, team_link)
+
+
+    
