86ce2ab8e0dd6768caa1bb13ab5c155199a85ef3
ZEEstranger <bidmexov98@gmail.com>
2025-05-28 00:11:06 +0300

    Update the team information collection function

---
+++ global_vars.py
@@ -0,0 +1,4 @@
+"Global variables Module"
+
+# Main url of wiki first page
+WIKI_SOURCE_URL = "https://liquipedia.net"
--- parsing_team.py
+++ parsing_team.py
@@ -1,12 +1,15 @@
-from playwright.sync_api import sync_playwright, Page
+""
+
 import re
+from playwright.sync_api import sync_playwright, Page
+from global_vars import WIKI_SOURCE_URL
 
 def parse_team_links() -> set:
-    base_url = "https://liquipedia.net"
-    teams_url = base_url + "/leagueoflegends/Portal:Teams"
+    teams_url = f"{WIKI_SOURCE_URL}/leagueoflegends/Portal:Teams"
 
     with sync_playwright() as pw:
         browser = pw.chromium.launch(headless=True)
+
         page = browser.new_page()
         page.goto(teams_url)
 
@@ -19,7 +22,9 @@ def parse_team_links() -> set:
         league_panels = page.query_selector_all("div.panel-box")
 
         for league_panel in league_panels:
-            team_league = league_panel.query_selector("div.panel-box-heading").query_selector("a").inner_text()
+            team_league = league_panel.query_selector(
+                "div.panel-box-heading"
+            ).query_selector("a").inner_text()
             print(team_league)
             team_names = league_panel.query_selector_all("span.team-template-text")
             for team_name in team_names:
@@ -37,52 +42,62 @@ def parse_team_links() -> set:
 
         return team_links, page
 
-def parse_team_info(page: Page, url):
-    base_url = "https://liquipedia.net"
-    page.goto(base_url + url)
-    page.wait_for_selector("div.fo-nttax-infobox-wrapper")
+def parse_team_info(brows_page: Page, url):
+    "Функция для получения подробной информации о команде"
 
-    name = None
-    region = None
-    founded = None
-    disbanded = None
+    brows_page.goto(WIKI_SOURCE_URL + url)
+    brows_page.wait_for_selector("div.fo-nttax-infobox-wrapper")
 
-    name = page.query_selector("div.infobox-header")
-    
-    name = name.inner_text()
-    cleaned = re.sub(r"\[.*?\]", "", name)
-    name = re.sub(r"\s+", " ", cleaned).strip()
 
+    # Название команды
+    name = brows_page.query_selector("div.infobox-header").inner_text()
+    name = re.sub(r"\s+", " ", re.sub(r"\[.*?\]", "", name)).strip()
+
+    # Дата создания команды
     section_counter = 1
-
-
-    date_box = ''
-    while date_box != 'History':
-        date_box = page.query_selector(f"div.fo-nttax-infobox > div:nth-of-type({section_counter}) > div")
-        date_box = date_box.inner_text()
+    created_date_box = ''
+    while created_date_box != 'History':
+        created_date_box = brows_page.query_selector(
+            f"div.fo-nttax-infobox > div:nth-of-type({section_counter}) > div"
+        ).inner_text()
         section_counter += 1
 
-    date_box = page.query_selector(f"div.fo-nttax-infobox > div:nth-of-type({section_counter}) > div:nth-of-type(2)")
-    date_box = date_box.inner_text()[-10:]
+    created_date_box = brows_page.query_selector(
+        f"div.fo-nttax-infobox > div:nth-of-type({section_counter}) > div:nth-of-type(2)"
+    ).inner_text()[-10:].replace("??", "01")
 
+    # Дата расформирования команды
+    disbanded_date = None
+    try:
+        disbanded_date = brows_page.query_selector(
+            f"div.fo-nttax-infobox > div:nth-of-type({section_counter + 1}) > div:nth-of-type(2)"
+        ).inner_text()[-10:].replace("??", "01")
+    except AttributeError:
+        pass
 
-    print(name, date_box)
+    print(name, created_date_box, disbanded_date)
 
 
 if __name__ == "__main__":
     team_links, pw_page = parse_team_links()
-    
+
     with sync_playwright() as pw:
         browser = pw.chromium.launch(headless=True)
         page = browser.new_page()
 
 
         cntr = 0
+        # ! dev
+        team_links = (
+            '/leagueoflegends/CompLexity_Gaming',
+            '/leagueoflegends/T1',
+            '/leagueoflegends/Gambit_Esports'
+        )
         for team_link in team_links:
             cntr += 1
             if not cntr % 5:
                 print(cntr)
-            a = parse_team_info(page, team_link)
+            parse_team_info(page, team_link)
 
 
     
